02bc993e2a8381adb68df1fbe8e6cc41166f9b53
golocule <129328765+golocule@users.noreply.github.com>
2024-12-02 20:34:56 -0500

    Update readData.h to store vector values

--- readData.h
+++ readData.h
@@ -36,48 +36,70 @@ public:
     vector<movie> getMovies() {
         ifstream movieFile("../tmdb_5000_movies.csv");
         vector<movie> movies;
-        movie currMovie;
         string line;
         int lineIndex = 0;
         while (getline(movieFile, line)) {
-            int i = 0;
-            //string currLine = line;
+            movie currMovie;
             vector<string> lists;
             vector<string> quotes;
-            string currList;
-            string currQuote;
-            for (char c : line) {
-                if (c == '[' or c == '"') {
-                    if (c == '[') {
-                        for (int k = i; k < line.length(); k++) {
-                            if (line[k] == ']') {
-                                line.erase(i, k-i+1);
-                                //i -= (k-i);
-                                //i--;
-                                break;
+            int i = 0;
+
+            if (lineIndex > 0) {
+                quotes.clear();
+                lists.clear();
+                string currList;
+                string currQuote;
+                for (char c : line) {
+                    if (c == '[' or c == '"') {
+                        if (c == '[') {
+                            for (int k = i+1; k < line.length(); k++) {
+                                if (line[k] == ']') {
+                                    line.erase(i, k-i+1);
+                                    break;
+                                }
+                                currList += line[k];
                             }
-                            currList += line[k];
-                        }
-                        lists.push_back(currList);
-                        currList = "";
-                    } else if (c == '"' and (isalpha(line[i+1]) or isdigit(line[i+1]) or line[i+1] == '(' or line[i+1] == '\'' or (line[i+1] == '"' and line[i+2] == '"') or line[i+1] == '.') and line[i-1] == ',') {
-                        //cout << "entered loop" << endl;
-                        //cout << line[i+1] << endl;
-                        for (int k = i+1; k < line.length(); k++) {
-                            if (line[k] == '"' and line[k+1] == ',' and line[k+2] != ' ') {
-                                line.erase(i, k-i+1);
-                                //i -= (k-i);
-                                //i--;
-                                break;
+                            if (currList != "") {
+                                lists.push_back(currList);
                             }
-                            currQuote += line[k];
+                            currList = "";
+                        } else if (c == '"' and (isalpha(line[i+1]) or isdigit(line[i+1]) or line[i+1] == '(' or line[i+1] == '\'' or (line[i+1] == '"' and line[i+2] == '"') or line[i+1] == '.') and line[i-1] == ',') {
+                            for (int k = i+1; k < line.length(); k++) {
+                                if (line[k] == '"' and line[k+1] == ',' and line[k+2] != ' ') {
+                                    line.erase(i, k-i+1);
+                                    break;
+                                }
+                                currQuote += line[k];
+                            }
+                            if (currQuote != "") {
+                                quotes.push_back(currQuote);
+                            }
+                            currQuote = "";
                         }
-                        //cout << currQuote << endl;
-                        quotes.push_back(currQuote);
-                        currQuote = "";
+                    }
+                    i++;
+                }
+
+                for (int j = 0; j < lists.size(); j++) {
+                    int pos = 0;
+                    string currStr = "";
+                    while ((pos = lists[j].find("""name""", pos)) != string::npos) {
+                        pos += 10;
+                        while (pos < lists[j].length() and lists[j][pos] != '"') {
+                            currStr += lists[j][pos];
+                            pos++;
+                        }
+                        if (j == 0) {
+                            currMovie.genres.push_back(currStr);
+                        } else if (j == 1) {
+                            currMovie.keywords.push_back(currStr);
+                        } else if (j == 2) {
+                            currMovie.productionComps.push_back(currStr);
+                        }
+                        currStr = "";
+                        pos += 6;
                     }
                 }
-                i++;
             }
 
             stringstream ss(line);
@@ -98,7 +120,9 @@ public:
                     } else if (i == 5) {
                         currMovie.language = element;
                     } else if (i == 7) {
-                        //currMovie.description = element;
+                        if (quotes.size() >= 1) {
+                            currMovie.description = quotes[0];
+                        }
                     } else if (i == 8) {
                         currMovie.popularity = stod(element);
                     } else if (i == 11) {
@@ -115,10 +139,10 @@ public:
                     i++;
                 }
             }
+
             lineIndex++;
             movies.push_back(currMovie);
         }
-
         return movies;
     }
 };
